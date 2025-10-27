811875cd3b7a04d152587cbe6d047757d733b0c4
pupozin <pupogabriel88@gmail.com>
2025-10-27 17:32:10 -0300

    Feat: Working in em andamento design

--- isonel-frontend/src/app/pages/em-andamento/em-andamento.ts
+++ isonel-frontend/src/app/pages/em-andamento/em-andamento.ts
@@ -15,9 +15,10 @@ export class EmAndamento {
   processoSelecionado: any = null;
 
   processos = [
-    { codigo: '00291', status: 'Em andamento', cor: 'orange', etapa: 'Venda', responsavel: 'Funcionário 1', produto: 'Câmara Frigorífica', cliente: 'Mercado Barbosa' },
-    { codigo: '00292', status: 'Finalizado', cor: 'green', etapa: 'Dobragens', responsavel: 'Funcionário 2', produto: 'Câmara Frigorífica', cliente: 'Dani Narguiles' },
-    { codigo: '00293', status: 'Pendente', cor: 'gray', etapa: 'Venda', responsavel: 'Funcionário 3', produto: 'Câmara Frigorífica', cliente: 'Paraty' },
+    { codigo: '00291', status: 'Em andamento', cor: 'orange', etapa: 'Venda', dataInicio: '27/10/2025 12:00', cliente: 'Mercado Barbosa', produto: 'Câmara Frigorífica', responsavel: 'Funcionário 1' },
+    { codigo: '00292', status: 'Finalizado', cor: 'green', etapa: 'Venda', dataInicio: '27/10/2025 14:00', cliente: 'Dani Narguiles', produto: 'Câmara Frigorífica', responsavel: 'Funcionário 2' },
+    { codigo: '00293', status: 'Pendente', cor: 'gray', etapa: 'Venda', dataInicio: '27/10/2025 09:00', cliente: 'Paraty', produto: 'Câmara Frigorífica', responsavel: 'Funcionário 3' },
+    { codigo: '00290', status: 'Finalizado', cor: 'green', etapa: 'Preparação', dataInicio: '25/10/2025 13:00', cliente: 'Dani Narguiles', produto: 'Câmara Frigorífica', responsavel: 'Funcionário 4' }
   ];
 
   etapas = [
@@ -34,7 +35,6 @@ export class EmAndamento {
     this.modalAberto = true;
   }
 
-  
   avancarEtapa(p: any) {
     this.processoSelecionado = p;
     this.modalAberto = true;
